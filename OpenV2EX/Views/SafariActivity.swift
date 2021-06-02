99e0cde24aacf53d944f80f40802455e013dcc92
fanchangyong <fanchangyong@gmail.com>
2021-06-02 18:00:25 +0800

    Change image size of safari

--- OpenV2EX/Views/SafariActivity.swift
+++ OpenV2EX/Views/SafariActivity.swift
@@ -19,7 +19,7 @@ class SafariActivity: UIActivity {
     }
 
     public override var activityImage: UIImage? {
-        return UIImage(systemName: "safari")
+        return UIImage(systemName: "safari", withConfiguration: UIImage.SymbolConfiguration(pointSize: 24))
     }
 
     public override func canPerform(withActivityItems activityItems: [Any]) -> Bool {
